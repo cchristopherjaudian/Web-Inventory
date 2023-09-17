21b671fd3eabffd769007da178857ce87ed02da2
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2023-09-17 20:11:34 +0800

    feat: Add custom Axios hook

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -1,41 +1,41 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+import customaxios from 'axios';
 
 function useAxios(url, method, requestData = null) {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const serviceId = process.env.REACT_APP_BASE_URL;
+  const axios = customaxios.create({
+    baseURL: serviceId,
+    timeout: 10000
+  });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                let response;
-                switch (method) {
-                    case 'GET':
-                        response = await axios.get(url);
-                        break;
-                    case 'POST':
-                        response = await axios.post(url, requestData);
-                        break;
-                    case 'PATCH':
-                        response = await axios.patch(url, requestData);
-                        break;
-                    default:
-                        throw new Error('Invalid method');
-                }
-                setData(response.data);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
-        };
+  const fetchData = async () => {
+    try {
+      let response;
+      switch (method) {
+        case 'GET':
+          response = await axios.get(url);
+          break;
+        case 'POST':
+          response = await axios.post(url, requestData);
+          break;
+        case 'PATCH':
+          response = await axios.patch(url, requestData);
+          break;
+        default:
+          throw new Error('Invalid method');
+      }
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        fetchData();
-    }, [url, method, requestData]);
-
-    return { data, loading, error };
+  return { data, loading, error, fetchData };
 }
 
 export default useAxios;
-//const { data, loading, error } = useAxios('https://api.example.com/data', 'POST', { key: 'value' });
