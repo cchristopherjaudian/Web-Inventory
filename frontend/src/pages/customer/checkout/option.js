7aea9fb0bcefb42fe689b15d701d236bef726240
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2023-10-06 19:24:32 +0800

    refactor: Add customer checkout assets

--- frontend/src/pages/customer/checkout/option.js
+++ frontend/src/pages/customer/checkout/option.js
@@ -9,17 +9,18 @@ import {
 } from '@mui/material';
 
 const Option = (props) => {
+   
     return (<CardActionArea>
         <Card sx={{ display: 'flex', height: 100 }}>
             <CardMedia
                 component="img"
-                sx={{ width: 100, height: 100, display: { xs: 'none', sm: 'block' } }}
-                image="https://placehold.co/100"
+                sx={{ m: 1, width: 80, height: 80, display: { xs: 'none', sm: 'block' } }}
+                image={props.img}
                 alt={props.name}
             />
             <CardContent sx={{ flex: 1, ml: -1, mt: -1 }}>
                 <Stack direction="column" justifyContent="center">
-                    <Typography component="caption" variant="caption" textAlign="left" sx={{ lineHeight: 2 }}>
+                    <Typography component="caption" variant="h6" textAlign="left" sx={{ lineHeight: 2 }}>
                         {props.name}
                     </Typography>
                     <Typography variant="caption" color="text.secondary" sx={{ lineHeight: 1, fontSize: '0.6rem' }}>
--- frontend/src/pages/customer/checkout/payment.js
+++ frontend/src/pages/customer/checkout/payment.js
@@ -6,16 +6,16 @@ import useAxios from 'hooks/useAxios';
 const Payment = (props) => {
 
     const methods = [
-        { id: 1, code: 'COD', name: 'Cash on Delivery', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet faucibus fringilla. In tristique at risus ut sagittis. Proin vel congue ante. Fusce ultrices arcu lectus,' },
-        { id: 2, code: 'GCASH', name: 'GCash', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet faucibus fringilla. In tristique at risus ut sagittis. Proin vel congue ante. Fusce ultrices arcu lectus,' },
-        { id: 3, code: 'BANK_TRANSFER', name: 'Bank Transfer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet faucibus fringilla. In tristique at risus ut sagittis. Proin vel congue ante. Fusce ultrices arcu lectus,' }
+        { id: 1, img: '/asset/cod.png', code: 'COD', name: 'Cash on Delivery', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet faucibus fringilla. In tristique at risus ut sagittis. Proin vel congue ante. Fusce ultrices arcu lectus,' },
+        { id: 2, img: '/asset/gcash.png', code: 'GCASH', name: 'GCash', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet faucibus fringilla. In tristique at risus ut sagittis. Proin vel congue ante. Fusce ultrices arcu lectus,' },
+        { id: 3, img: '/asset/bank.png', code: 'BANK_TRANSFER', name: 'Bank Transfer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet faucibus fringilla. In tristique at risus ut sagittis. Proin vel congue ante. Fusce ultrices arcu lectus,' }
     ]
 
     return (<Grid container spacing={1}>
         {
             methods.map((method, index) => {
                 return <Grid item xs={12} key={index} onClick={() => props.setPayMethod(method.code)}>
-                    <Option name={method.name} description={method.description} />
+                    <Option name={method.name} description={method.description} img={method.img} />
                 </Grid>;
 
             })
