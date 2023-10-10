41b4db89d1d0a14527e0804ec87bf9a17530707e
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2023-10-10 11:22:31 +0800

    refactor: Change SubAdmin1 AccountTable to OrderTable

--- frontend/src/pages/subadmin1/orders/OrderTable.js
+++ frontend/src/pages/subadmin1/orders/OrderTable.js
@@ -3,10 +3,8 @@ import {RightOutlined} from '@ant-design/icons';
 import {
     Box,
     Button,
-    Grid,
-    Snackbar
+    Grid
 } from '@mui/material';
-import MuiAlert from '@mui/material/Alert';
 import useAxios from 'hooks/useAxios';
 import useInventoryAxios from 'hooks/useInventoryAxios';
 import { useState, useEffect, forwardRef } from 'react';
