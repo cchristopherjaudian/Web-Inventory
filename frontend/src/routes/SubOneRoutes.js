eb4218457f2d732dcae39a462eda9edc4a3260ea
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2023-10-10 11:24:20 +0800

    feat: Add SubAdmin1 route

--- frontend/src/routes/SubOneRoutes.js
+++ frontend/src/routes/SubOneRoutes.js
@@ -2,34 +2,39 @@ import { lazy } from 'react';
 
 import Loadable from 'components/Loadable';
 import MainLayout from 'layout/MainLayout';
+import { element } from 'prop-types';
 
 
 const TransactionRecords = Loadable(lazy(() => import('pages/subadmin1/transaction/')));
 const OrderNotice = Loadable(lazy(() => import('pages/subadmin1/ordernotice/')));
 const Orders = Loadable(lazy(() => import('pages/subadmin1/orders/')));
-
+const OrderInfo = Loadable(lazy(() => import('pages/subadmin1/ordernotice/orderinfo')));
 
 const SubOneRoutes = {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        path: '/',
-        element: <TransactionRecords />
-      },
-      {
-        path: 'transactions',
-        element: <TransactionRecords />
-      },
-      {
-        path: 'notice',
-        element: <OrderNotice />
-      },
-      {
-        path: 'orders',
-        element: <Orders />
-      }
-    ]
-  };
-  
-  export default SubOneRoutes;
+  path: '/',
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <TransactionRecords />
+    },
+    {
+      path: 'transactions',
+      element: <TransactionRecords />
+    },
+    {
+      path: 'notice',
+      element: <OrderNotice />
+    },
+    {
+      path: 'orders',
+      element: <Orders />
+    },
+    {
+      path: 'order/:id',
+      element: <OrderInfo />
+    }
+  ]
+};
+
+export default SubOneRoutes;
