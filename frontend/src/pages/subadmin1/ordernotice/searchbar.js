e357ce325760afcf59a8feb19a7112a53427c188
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2023-10-13 17:00:12 +0800

    refactor: Remove ordernotice searchbar

--- frontend/src/pages/subadmin1/ordernotice/searchbar.js
+++ frontend/src/pages/subadmin1/ordernotice/searchbar.js
@@ -4,10 +4,6 @@ import { SearchOutlined } from '@ant-design/icons';
 
 const Searchbar = (props) => {
     return (<Grid item xs={12} sx={{ display: 'flex' }} gap={1}>
-        <ButtonGroup variant="outlined" aria-label="outlined button group">
-            <Button size="small" onClick={()=>props.setFilter(1)}>All</Button>
-            <Button size="small" onClick={()=>props.setFilter(0)}>Unread</Button>
-        </ButtonGroup>
         <FormControl sx={{ width: { xs: '100%', ml: 1 } }}>
             <OutlinedInput
                 size="small"
