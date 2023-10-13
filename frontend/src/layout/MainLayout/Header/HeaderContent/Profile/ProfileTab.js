79a8ec41239713e725bd2ce2e50b15e941d97366
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2023-10-13 16:55:18 +0800

    refactor: Move logout button to profile tab

--- frontend/src/layout/MainLayout/Header/HeaderContent/Profile/ProfileTab.js
+++ frontend/src/layout/MainLayout/Header/HeaderContent/Profile/ProfileTab.js
@@ -1,40 +1,46 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
+import { setToken,setAuth } from 'store/reducers/token';
 import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
 import { EditOutlined, ProfileOutlined, LogoutOutlined, UserOutlined, WalletOutlined } from '@ant-design/icons';
 
 
-const ProfileTab = ({ handleLogout }) => {
+const ProfileTab = () => {
   const theme = useTheme();
-
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [selectedIndex, setSelectedIndex] = useState(0);
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
   };
 
+  const handleLogout = () => {
+    dispatch(setToken(''));
+    dispatch(setAuth(false));
+    navigate('/',{replace:true});
+  };
+  const goToProfile = () =>{
+    navigate('/profile');
+  }
   return (
     <List component="nav" sx={{ p: 0, '& .MuiListItemIcon-root': { minWidth: 32, color: theme.palette.grey[500] } }}>
-      <ListItemButton selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
+      <ListItemButton selected={selectedIndex === 0} onClick={goToProfile}>
         <ListItemIcon>
           <EditOutlined />
         </ListItemIcon>
         <ListItemText primary="Edit Profile" />
       </ListItemButton>
-      <ListItemButton selected={selectedIndex === 1} onClick={(event) => handleListItemClick(event, 1)}>
+      <ListItemButton onClick={handleLogout}>
         <ListItemIcon>
-          <UserOutlined />
+          <LogoutOutlined />
         </ListItemIcon>
-        <ListItemText primary="View Profile" />
+        <ListItemText primary="Logout" />
       </ListItemButton>
     </List>
   );
 };
-
-ProfileTab.propTypes = {
-  handleLogout: PropTypes.func
-};
-
 export default ProfileTab;
