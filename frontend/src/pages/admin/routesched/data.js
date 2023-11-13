ea314916d71d3c79b36457547ad424cd532afd23
cliljs <calilchristopher052997@gmail.com>
2023-11-13 21:29:28 +0800

    BACKEND: refactor: modified optional field for time and date in schedule routes
    FRONTEND:
    refactor: fix field for schedule update
    refactor: added fallback for schedule rendering

--- frontend/src/pages/admin/routesched/data.js
+++ frontend/src/pages/admin/routesched/data.js
@@ -1,85 +1,76 @@
-import {
-    Card,
-    CardActionArea,
-    CardContent,
-    Grid,
-    Stack,
-    Typography,
-    IconButton,
-    TextField
-} from '@mui/material';
+import { Card, CardActionArea, CardContent, Grid, Stack, Typography, IconButton, TextField } from '@mui/material';
 import { useState, useEffect } from 'react';
-import { EditOutlined, SaveOutlined } from "@ant-design/icons"
+import { EditOutlined, SaveOutlined } from '@ant-design/icons';
 import useAxios from 'hooks/useAxios';
 import Swal from 'sweetalert2';
 import { useSelector } from 'react-redux';
 const Data = (props) => {
-    const isAdmin = useSelector((state) => state.token.isadmin.isadmin);
+  const isAdmin = useSelector((state) => state.token.isadmin.isadmin);
 
-    const [edit, setEdit] = useState(false);
-    const [title, setTitle] = useState(props.title);
-    const [rowId, setRowId] = useState(props.rowId);
-    const [field, setField] = useState(props.field);
-    const { fetchData } = useAxios(`schedules/${rowId}`, 'PATCH', { [props.field]: title });
-    return (
-        <Grid item xs={6} sx={{ mt: 0.5 }}>
-            <CardActionArea>
-                <Card sx={{ backgroundColor: props.background ? props.background : 'white' }}>
-                    <IconButton
-                        aria-label="edit"
-                        sx={{ position: 'absolute', top: 0, right: 0 }}
-                        onClick={() => {
-                          
-                            if (edit) {
-                                if (!title) {
-                                    Swal.fire({
-                                        icon: 'info',
-                                        title: 'Route Schedule',
-                                        text: 'Please input a valid description',
-                                        allowOutsideClick: false,
-                                        confirmButtonText: 'Ok'
-                                    });
-                                    return;
-                                } else {
-                                    fetchData();
-                                }
-
-                            }
-                            setEdit(!edit)
-                        }}
-                    >
-                        {
-                            edit ? <SaveOutlined /> : (isAdmin) && <EditOutlined />
-                        }
-                    </IconButton>
-                    <CardContent sx={{ flex: 1, ml: -1, mt: -1 }}>
-                        <Stack direction="row" justifyContent="center" alignItems="center" spacing={5} sx={{ px: 2 }}>
-                            {
-                                edit ?
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="routeField"
-                                        label=""
-                                        name="routeField"
-                                        value={title}
-                                        onChange={(event) => {
-                                            setTitle(event.target.value);
-                                        }}
-                                        autoFocus
-                                    />
-                                    :
-                                    <Typography component="h6" variant="h6" textAlign="center" sx={{ color: props.color ? props.color : 'black' }} >
-                                        {title}
-                                    </Typography>
-                            }
-
-                        </Stack>
-                    </CardContent>
-                </Card>
-            </CardActionArea>
-        </Grid >
-    );
-}
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState(props.title);
+  const [rowId, setRowId] = useState(props.rowId);
+  const [field, setField] = useState(props.field);
+  const { fetchData } = useAxios(`schedules/${rowId}`, 'PATCH', { [props.field]: title });
+  return (
+    <Grid item xs={6} sx={{ mt: 0.5 }}>
+      <CardActionArea>
+        <Card sx={{ backgroundColor: props.background ? props.background : 'white' }}>
+          <IconButton
+            aria-label="edit"
+            sx={{ position: 'absolute', top: 0, right: 0 }}
+            onClick={() => {
+              if (edit) {
+                if (!title) {
+                  Swal.fire({
+                    icon: 'info',
+                    title: 'Route Schedule',
+                    text: 'Please input a valid description',
+                    allowOutsideClick: false,
+                    confirmButtonText: 'Ok'
+                  });
+                  return;
+                } else {
+                  fetchData();
+                }
+              }
+              setEdit(!edit);
+            }}
+          >
+            {edit ? <SaveOutlined /> : isAdmin && <EditOutlined />}
+          </IconButton>
+          <CardContent sx={{ flex: 1, ml: -1, mt: -1 }}>
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={5} sx={{ px: 2 }}>
+              {edit ? (
+                <TextField
+                  required
+                  fullWidth
+                  id="routeField"
+                  label=""
+                  name="routeField"
+                  value={title}
+                  onChange={(event) => {
+                    setTitle(event.target.value);
+                  }}
+                  autoFocus
+                />
+              ) : (
+                <Typography
+                  component="h6"
+                  variant="h6"
+                  textAlign="center"
+                  sx={{ color: props.color ? props.color : 'black' }}
+                  style={{ whiteSpace: 'pre', textAlign: 'justify' }}
+                >
+                  {title}
+                </Typography>
+              )}
+            </Stack>
+          </CardContent>
+        </Card>
+      </CardActionArea>
+    </Grid>
+  );
+};
 
 export default Data;
--- frontend/src/pages/admin/routesched/index.js
+++ frontend/src/pages/admin/routesched/index.js
@@ -18,17 +18,20 @@ const RouteSched = () => {
       const newSpap = [];
       const newTruck = [];
 
-      data['data'].map((s, i) => {
-        newRoutes.push({ route: s['route'], id: s['id'] });
-        newSched.push({ sched: s['date'], id: s['id'] });
-        newSpap.push({ spap: s['description'], id: s['id'] });
-        newTruck.push({ plate: s['plateNumber'], id: s['id'] });
-      });
-      setListRoutes(newRoutes);
-      setListSchedule(newSched);
-      setListspap(newSpap);
-      setListtruck(newTruck);
-      console.log(listRoutes);
+      console.log('data', data);
+      if (data?.data.length > 0) {
+        data['data'].forEach((s, i) => {
+          newRoutes.push({ route: s?.route, id: s?.id });
+          newSched.push({ description: s?.description, id: s?.id });
+          newSpap.push({ spap: s?.profile?.fullName || 'N/A', id: s?.id });
+          newTruck.push({ plate: s?.plateNumber, id: s?.id });
+        });
+        setListRoutes(newRoutes);
+        setListSchedule(newSched);
+        setListspap(newSpap);
+        setListtruck(newTruck);
+        console.log(listRoutes);
+      }
     }
   }, [data]);
   return (
@@ -37,30 +40,43 @@ const RouteSched = () => {
         <Grid item xs={12} lg={3} direction="column">
           <Header title="ROUTES" img="/asset/routes.png" />
           {listRoutes.map((item, index) => {
-            return <Data field='route' title={item.route} rowId={item.id} key={index} background={(index % 2 === 0) ? '#f2f2f2' : 'white'} />
+            return <Data field="route" title={item.route} rowId={item.id} key={index} background={index % 2 === 0 ? '#f2f2f2' : 'white'} />;
           })}
         </Grid>
         <Grid item xs={12} lg={3} direction="column">
           <Header title="SCHEDULE" img="./asset/schedule.png" />
           {listSchedule.map((item, index) => {
-            return <Data field='date' title={item.sched} rowId={item.id} key={index} color='red' background={(index % 2 === 0) ? '#f2f2f2' : 'white'} />
+            return (
+              <Data
+                field="description"
+                title={item?.description}
+                rowId={item.id}
+                key={index}
+                color="red"
+                background={index % 2 === 0 ? '#f2f2f2' : 'white'}
+              />
+            );
           })}
         </Grid>
         <Grid item xs={12} lg={3} direction="column">
           <Header title="SP/AP" img="./asset/03.png" />
           {listspap.map((item, index) => {
-            return <Data field='description' title={item.spap} rowId={item.id} key={index} background={(index % 2 === 0) ? '#f2f2f2' : 'white'} />
+            return (
+              <Data field="description" title={item.spap} rowId={item.id} key={index} background={index % 2 === 0 ? '#f2f2f2' : 'white'} />
+            );
           })}
         </Grid>
         <Grid item xs={12} lg={3} direction="column">
           <Header title="TRUCK" img="./asset/truck.png" />
           {listtruck.map((item, index) => {
-            return <Data field='plateNumber' title={item.plate} rowId={item.id} key={index} background={(index % 2 === 0) ? '#f2f2f2' : 'white'} />
+            return (
+              <Data field="plateNumber" title={item.plate} rowId={item.id} key={index} background={index % 2 === 0 ? '#f2f2f2' : 'white'} />
+            );
           })}
         </Grid>
-      </Grid >
-
-    </MainCard >);
+      </Grid>
+    </MainCard>
+  );
 };
 
 export default RouteSched;
