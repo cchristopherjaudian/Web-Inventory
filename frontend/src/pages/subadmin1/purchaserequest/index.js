676f8f03cc3900a3e2b38e3590c7a3b4e8400e1e
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2024-02-19 20:28:59 +0800

    feat: Add Axios call to PR List

--- frontend/src/pages/subadmin1/purchaserequest/index.js
+++ frontend/src/pages/subadmin1/purchaserequest/index.js
@@ -2,8 +2,18 @@ import { Grid, Button } from '@mui/material';
 import MainCard from 'components/MainCard';
 import OrderTable from "./OrderTable";
 import Searchbar from "./searchbar";
-
+import useAxios from 'hooks/useAxios';
+import { useEffect, useState } from 'react';
 const PurchaseRequest = () => {
+
+    const [prList,setPrList] = useState([]);
+    const {data} = useAxios('purchase','GET',null,false);
+
+    useEffect(()=>{
+        if(data){
+            console.log(data);
+        }
+    },[data]);
     return (
         <MainCard title="Purchase Request List">
             <Grid container spacing={1}>
