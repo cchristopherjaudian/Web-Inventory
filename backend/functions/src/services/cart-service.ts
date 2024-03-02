0321d63ccd0517e908a2ebf9bca90b45d29465f9
Calil Christopher Jaudian <cchristopherjaudian@Calils-MacBook-Air.local>
2024-03-02 20:43:05 +0800

    feat: added handling if cart does not exists in addCart build: created new schema for Quantity Indicator

--- backend/functions/src/services/cart-service.ts
+++ backend/functions/src/services/cart-service.ts
@@ -1,124 +1,121 @@
 import { TQueryArgs } from '../../index';
 import {
-    BadRequestError,
-    NotFoundError,
+  BadRequestError,
+  NotFoundError,
 } from '../lib/custom-errors/class-errors';
 import { TCart, TCartList } from '../lib/types/cart-types';
 import { TPrismaClient } from '../lib/prisma';
 
 class CartService {
-    private _db: TPrismaClient;
+  private _db: TPrismaClient;
 
-    constructor(db: TPrismaClient) {
-        this._db = db;
-    }
+  constructor(db: TPrismaClient) {
+    this._db = db;
+  }
 
-    public async addCart(payload: TCart & { code?: string }) {
-        try {
-            const product = await this._db.products.findFirst({
-                where: {
-                    code: payload.code,
-                },
-            });
-            if (!product) {
-                throw new NotFoundError('Products does not exists.');
-            }
+  public async addCart(payload: TCart & { code?: string }) {
+    try {
+      const product = await this._db.products.findFirst({
+        where: {
+          code: payload.code,
+        },
+      });
+      if (!product) {
+        throw new NotFoundError('Products does not exists.');
+      }
 
-            let cart;
-            if (payload.groupNo) {
-                cart = await this._db.cart.findFirst({
-                    where: {
-                        profileId: payload.profileId,
-                        groupNo: payload.groupNo,
-                        productId: product?.id,
-                    },
-                });
-            } else {
-                cart = await this._db.cart.findFirst({
-                    where: {
-                        profileId: payload.profileId,
-                        productId: product?.id,
-                    },
-                });
-            }
-
-            const inventories = await this._db.inventory.findMany({
-                where: {
-                    productId: product.id,
-                },
-                orderBy: [
-                    {
-                        expiration: 'asc',
-                    },
-                ],
-            });
-
-            const stockTotal = inventories.reduce(
-                (acc, curr) => acc + curr.stock,
-                0
-            );
-            if (payload.quantity > stockTotal) {
-                throw new BadRequestError('Insufficient stock.');
-            }
-
-            if (
-                cart &&
-                (!payload.groupNo || payload.groupNo === cart.groupNo)
-            ) {
-                cart.quantity = cart.quantity + payload.quantity;
-                return this._db.cart.update({
-                    where: { id: cart.id },
-                    data: cart,
-                });
-            }
-
-            if (payload?.code) {
-                delete payload?.code;
-            }
-            payload.productId = product.id;
-            return this._db.cart.create({ data: payload });
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    public async getUserCart(params: TCartList) {
-        try {
-            const query = {
-                where: { profileId: params.profileId },
-                select: {
-                    id: true,
-                    quantity: true,
-                    products: true,
-                },
-            } as TQueryArgs;
-
-            return this._db.cart.findMany(query);
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    public async getCartItem(id: string) {
-        return this._db.cart.findFirst({
-            where: { id },
-            select: {
-                id: true,
-                quantity: true,
-                products: true,
-            },
+      let cart;
+      if (payload.groupNo) {
+        cart = await this._db.cart.findFirst({
+          where: {
+            profileId: payload.profileId,
+            groupNo: payload.groupNo,
+            productId: product?.id,
+          },
         });
-    }
+      } else {
+        cart = await this._db.cart.findFirst({
+          where: {
+            profileId: payload.profileId,
+            productId: product?.id,
+          },
+        });
+      }
 
-    public async updateCart(id: string, payload: Partial<TCart>) {
-        const cart = await this.getCartItem(id);
-        if (!cart) throw new NotFoundError('Cart item does not exists.');
-        return this._db.cart.update({ where: { id }, data: payload });
-    }
+      if (!cart) throw new BadRequestError('Cart does not exists.');
 
-    public async deleteCartItem(id: string) {
-        return this._db.cart.delete({ where: { id } });
+      const inventories = await this._db.inventory.findMany({
+        where: {
+          productId: product.id,
+        },
+        orderBy: [
+          {
+            expiration: 'asc',
+          },
+        ],
+      });
+
+      const stockTotal = inventories.reduce((acc, curr) => acc + curr.stock, 0);
+      if (payload.quantity > stockTotal) {
+        throw new BadRequestError('Insufficient stock.');
+      }
+
+      if (!payload.groupNo || payload.groupNo === cart.groupNo) {
+        cart.quantity = cart.quantity + payload.quantity;
+
+        return this._db.cart.update({
+          where: { id: cart.id },
+          data: cart,
+        });
+      }
+
+      if (payload?.code) {
+        delete payload?.code;
+      }
+      payload.productId = product.id;
+      return this._db.cart.create({ data: payload });
+    } catch (error) {
+      throw error;
     }
+  }
+
+  public async getUserCart(params: TCartList) {
+    try {
+      const query = {
+        where: { profileId: params.profileId },
+        select: {
+          id: true,
+          quantity: true,
+          products: true,
+        },
+      } as TQueryArgs;
+
+      return this._db.cart.findMany(query);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async getCartItem(id: string) {
+    return this._db.cart.findFirst({
+      where: { id },
+      select: {
+        id: true,
+        quantity: true,
+        products: true,
+      },
+    });
+  }
+
+  public async updateCart(id: string, payload: Partial<TCart>) {
+    const cart = await this.getCartItem(id);
+    if (!cart) throw new NotFoundError('Cart item does not exists.');
+    return this._db.cart.update({ where: { id }, data: payload });
+  }
+
+  public async deleteCartItem(id: string) {
+    return this._db.cart.delete({ where: { id } });
+  }
 }
 
 export default CartService;
